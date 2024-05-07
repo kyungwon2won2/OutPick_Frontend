8b58a06049af7c79a4a399d679046b7e804a5acd
YoungWon <nbbb9@naver.com>
2024-05-07 14:35:50 +0900

    2

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let test_axios = axios.create({
-    baseURL : "http://localhost:8080/",
+    baseURL : "http://localhost:8972/",
     headers: {
         'Content-Type': 'application/json; charset=utf-8'
     }
